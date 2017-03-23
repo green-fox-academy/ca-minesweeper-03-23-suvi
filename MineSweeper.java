0b48d65c0209c547c2407e45cb9653187645392d
Suviii <istvan.suvoltos@gmail.com>
2017-03-23 15:11:41 +0100

    add mine-indicators

--- MineSweeper.java
+++ MineSweeper.java
@@ -4,10 +4,10 @@ public class MineSweeper {
   static final int MAPWIDTH = 10;
   static final int MAPHEIGHT = 10;
   static final int MINESUM = 10;
+  static ArrayList<Integer> mineMap;
 
   public static void main(String[] args) {
     fillMap(initMap());
-    System.out.println(checkMine(initMap(), 0)); 
   }
 
   public static ArrayList<Integer> initMap() {
@@ -20,16 +20,17 @@ public class MineSweeper {
     return initializedmap;  
   }
 
-  public static ArrayList<Integer> fillMap(ArrayList<Integer> map) {    
+  public static ArrayList<Integer> fillMap(ArrayList<Integer> map) { 
     Random rand = new Random();
     for (int i = 0; i < MINESUM;) {
       int  n = rand.nextInt(MAPWIDTH * MAPHEIGHT);
       if (checkMine(map, n)) {
+        map = addIndicator(map, n);
         map.set(n, 9);
         i++;
       }
     }    
-    return new ArrayList<>();
+    return map;
   }
 
   public static boolean checkMine(ArrayList<Integer> map, int item) {    
@@ -39,5 +40,18 @@ public class MineSweeper {
     else {
       return true;
     }
+  }
+
+  public static ArrayList<Integer> addIndicator(ArrayList<Integer> map, int item) {
+    for (int i = -1; i < 2; i++) {
+      for (int j = -1; j < 2; j++) {
+        int selecteditem = item + 1 * i + MAPWIDTH * j;
+        if (selecteditem >= 0 && selecteditem < MAPWIDTH * MAPHEIGHT) {
+          int itemvalue = map.get(selecteditem);
+          map.set(selecteditem, itemvalue++);
+        }
+      }
+    }
+    return map;
   }
 }
