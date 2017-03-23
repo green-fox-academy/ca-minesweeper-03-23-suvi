f632b94a5524b6c51524191542a3f3a18f25aaa1
Suviii <istvan.suvoltos@gmail.com>
2017-03-23 14:38:28 +0100

    add mine placement

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,20 +1,30 @@
 import java.util.*;
 
-public class MineSweeper {
-  public static void main(String[] args) {
-    final int mapWidth = 10;
-    final int mapHeight = 10;
-    ArrayList<Integer> Map = generateMap(mapWidth, mapHeight);
+public class MineSweeper {  
+  static final int MAPWIDTH = 10;
+  static final int MAPHEIGHT = 10;
+  static final int MINESUM = 10;
 
+  public static void main(String[] args) {
+    fillMap(initMap());
   }
 
-  public static ArrayList<Integer> initMap(int width, int height) {
+  public static ArrayList<Integer> initMap() {
     ArrayList<Integer> initializedmap = new ArrayList<>();
-    for (int i = 0; i < width; i++) {
-        for (int j = 0; j < height; j++) {
-            initializedmap.add(0);
-        }
+    for (int i = 0; i < MAPWIDTH; i++) {
+      for (int j = 0; j < MAPHEIGHT; j++) {
+        initializedmap.add(0);
+      }
     }
     return initializedmap;  
+  }
+
+  public static ArrayList<Integer> fillMap(ArrayList<Integer> map) {    
+    Random rand = new Random();
+    for (int i = 0; i < MINESUM; i++) {
+      int  n = rand.nextInt(MAPWIDTH * MAPHEIGHT);
+      map.set(n, 9);
+    }
+    return new ArrayList<>();
   }
 }
