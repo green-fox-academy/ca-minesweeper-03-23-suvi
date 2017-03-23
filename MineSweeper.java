eddcef2df37aaed11664d7782a76b5f71cfa9b8b
Suviii <istvan.suvoltos@gmail.com>
2017-03-23 14:26:12 +0100

    add init map

--- MineSweeper.java
+++ MineSweeper.java
@@ -1,7 +1,20 @@
 import java.util.*;
 
 public class MineSweeper {
-    public static void main(String[] args) {
-        
+  public static void main(String[] args) {
+    final int mapWidth = 10;
+    final int mapHeight = 10;
+    ArrayList<Integer> Map = generateMap(mapWidth, mapHeight);
+
+  }
+
+  public static ArrayList<Integer> initMap(int width, int height) {
+    ArrayList<Integer> initializedmap = new ArrayList<>();
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            initializedmap.add(0);
+        }
     }
+    return initializedmap;  
+  }
 }
